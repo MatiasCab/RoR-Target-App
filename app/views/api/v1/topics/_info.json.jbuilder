35d915faaa40f5084828359049573d83f5f6d5dd
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-05-09 12:08:08 -0300

    topics index rspec

--- app/views/api/v1/topics/_info.json.jbuilder
+++ app/views/api/v1/topics/_info.json.jbuilder
@@ -1,2 +1,5 @@
-json.name       topic.name
-json.image      topic.image
+json.id             topic.id
+json.name           topic.name
+json.image          topic.image
+json.created_at     topic.created_at
+json.updated_at     topic.updated_at
--- spec/requests/api/v1/topics/index_spec.rb
+++ spec/requests/api/v1/topics/index_spec.rb
@@ -1,6 +1,6 @@
 describe 'GET api/v1/topics', type: :request do
-    let(:user)      { create(:user) }
-    let(:topics)    { create_list(:topic, 10).as_json(only: [:name, :image]) }
+    let!(:user)      { create(:user) }
+    let!(:topics)    { create_list(:topic, 10) }
     subject { get api_v1_topics_path, headers: auth_headers, as: :json }
 
     it "returns a successful response" do
@@ -8,8 +8,19 @@ describe 'GET api/v1/topics', type: :request do
         expect(response).to be_successful
     end
 
+    it "returns all topics" do
+        subject
+        expect(json[:topics].pluck(:name)).to match_array(topics.pluck(:name))
+    end
+
+
     it "returns topics in correct format" do
         subject
-        expect(json[:topics]).to match_array(topics)
+        expect(json[:topics].first).to include_json(
+            id: campaign.id,
+            title: campaign.title,
+            description: campaign.description,
+            compensation_offer: campaign.compensation_offer
+        )
     end
 end

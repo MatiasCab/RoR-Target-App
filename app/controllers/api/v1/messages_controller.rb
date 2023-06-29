0d8af5f99a10785a31484cf7fed683d86bd1645b
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-06-29 16:40:11 -0300

    Some comments deleted

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -3,7 +3,6 @@ module Api
     class MessagesController < Api::V1::ApiController
       def create
         authorize Message
-        # @message = conversation_messages.create!(resource_params)
         @message = CreateMessageService.new(params, current_user).create!
       end
     end

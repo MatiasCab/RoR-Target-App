aca80e035f2d2d812aa409f0ab85e78989dfd97b
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-07-04 15:51:16 -0300

    Chnages of review

--- app/services/create_message_service.rb
+++ app/services/create_message_service.rb
@@ -1,4 +1,6 @@
 class CreateMessageService
+  attr_reader :current_user, :params
+
   def initialize(params, current_user)
     @current_user = current_user
     @params = params
@@ -7,7 +9,7 @@ class CreateMessageService
   def create!
     message_info = {
       content: resource_params[:content],
-      user_id: @current_user.id
+      user_id: current_user.id
     }
     conversation_messages.create!(message_info)
   end
@@ -15,10 +17,10 @@ class CreateMessageService
   private
 
   def conversation_messages
-    @conversation_messages ||= @current_user.conversations.find(@params[:conversation_id]).messages
+    @conversation_messages ||= current_user.conversations.find(params[:conversation_id]).messages
   end
 
   def resource_params
-    @resource_params ||= @params.require(:message).permit(:content)
+    @resource_params ||= params.require(:message).permit(:content)
   end
 end

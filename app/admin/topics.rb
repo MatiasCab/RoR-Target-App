6bc1ac39e183be707ad8ac55855525b820a118c8
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-07-09 19:37:19 -0300

    test ready

--- app/admin/topics.rb
+++ app/admin/topics.rb
@@ -1,30 +1,30 @@
-ActiveAdmin.register Topic do  
-    permit_params :name, :image
+ActiveAdmin.register Topic do
+  permit_params :name, :image
 
-    form do |f|
-        f.inputs 'Details' do
-          f.input :name
-          f.input :image
-        end
-        actions
-      end
-
-    index do
-      id_column
-      column :name
-      column :image do |topic|
-        image_tag topic.image, height: '25px' if topic.image.present?
-      end
-      actions
+  form do |f|
+    f.inputs 'Details' do
+      f.input :name
+      f.input :image
     end
-    
-    show do
-      attributes_table do
-        row :id
-        row :name
-        row :image do |topic|
-          image_tag topic.image, height: '25px' if topic.image.present?
-        end
+    actions
+  end
+
+  index do
+    id_column
+    column :name
+    column :image do |topic|
+      image_tag topic.image, height: '25px'
+    end
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :image do |topic|
+        image_tag topic.image, height: '25px'
       end
     end
   end
+end

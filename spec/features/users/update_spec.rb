e34c8a3e1826a7d3ad0728b0d01efd3e7b181ee5
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-07-09 21:29:36 -0300

    Ready v2

--- spec/features/users/update_spec.rb
+++ spec/features/users/update_spec.rb
@@ -16,23 +16,4 @@ RSpec.describe 'Admin Users Edit' do
     expect(page).to have_field('Username', with: user.username)
     expect(page).to have_select('Plan*', selected: user.plan.name)
   end
-
-  it "updates the user's details when submitting the form" do
-    subject
-    new_email = 'new_email@example.com'
-    new_first_name = 'John'
-    new_last_name = 'Doe'
-
-    fill_in 'Email*', with: new_email
-    fill_in 'First name', with: new_first_name
-    fill_in 'Last name', with: new_last_name
-    select new_plan.name, from: 'Plan*'
-    click_button 'Update User'
-
-    # expect(User.first.email).to eq(new_email)
-    # expect(User.first.first_name).to eq(new_first_name)
-    # expect(User.first.last_name).to eq(new_last_name)
-    # expect(User.first.username).to eq(new_username)
-    # expect(User.first.plan).to eq(new_plan)
-  end
 end

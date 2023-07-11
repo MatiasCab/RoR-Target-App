4d4c2d06951b8e21343dcf067a79acfac4a46e05
Matias Cabrera <88689998+MatiasCab@users.noreply.github.com>
2023-07-11 11:46:48 -0300

    Create tests

--- spec/features/users/create_spec.rb
+++ spec/features/users/create_spec.rb
@@ -16,4 +16,24 @@ describe 'Admin create user', type: :feature do
     expect(page).to have_field('Password')
     expect(page).to have_field('Password confirmation')
   end
+
+  
+  it "create the user" do
+      subject
+      fill_in 'Email', with: 'test@example.com'
+      fill_in 'First name', with: 'test'
+      fill_in 'Last name', with: 'test'
+      fill_in 'Username', with: 'test'
+      fill_in 'Password*', with: 'password123'
+      fill_in 'Password confirmation', with: 'password123'
+      binding.pry
+      click_button 'Create User'  #Does not func
+      new_user = User.last
+      binding.pry
+
+      expect(new_user.email).to eq('test@example.com')
+      expect(new_user.first_name).to eq('test')
+      expect(new_user.last_name).to eq('test')
+      expect(new_user.username).to eq('test')
+      end
 end
